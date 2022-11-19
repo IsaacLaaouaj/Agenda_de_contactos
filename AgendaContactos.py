f6056dc52f34fc1237a2153a2efb4ecb272a2e95
Isaac <isaac.laaouaj@gmail.com>
2022-11-19 19:50:43 +0100

    Finalizamos conexión a la base de datos y terminamos la función "add_contact"

--- AgendaContactos.py
+++ AgendaContactos.py
@@ -4,17 +4,27 @@ def add_contact():
     name = input("Introduce el nombre del contacto: ")
     surname = input("Introduce el apellido: ")
     phone_number = input("Introduce el número de teléfono: ")
-    adress = input("Introduce la dirección: ")
+    address = input("Introduce la dirección: ")
     mail = input("Indroduce el correo electrónico: ")
+    ref.push({
+        'Nombre':name,
+        'Apellido': surname,
+        'Número de teléfono': phone_number,
+        'Direccion' : address,
+        'Correo electronico' : mail
+    })
 
 def delete_contact():
     remove_contact = input("Introduce el contacto que quieres borrar: ")
+
+
+credenciales = credentials.Certificate('agenda-1720b-firebase-adminsdk-nbaiz-0e38f4eba0.json')
+firebase_admin.initialize_app(credential=credenciales, options={"databaseURL" : "https://agenda-1720b-default-rtdb.europe-west1.firebasedatabase.app/"})
+ref = db.reference()
 
 switcher = {
     1: add_contact,
     2: delete_contact
 }
-menu = input("Seleccione las opciones:\n1: Añadir contacto \n2: Borrar contactos")
-
-credenciales = credentials.Certificate('agenda-1720b-firebase-adminsdk-nbaiz-0e38f4eba0.json')
-firebase_admin.initialize_app(credential=credenciales, options={"databaseURL" : "https://agenda-1720b-default-rtdb.europe-west1.firebasedatabase.app/"})
+menu = int(input("Seleccione las opciones:\n1: Añadir contacto \n2: Borrar contactos"))
+switcher[menu]()
