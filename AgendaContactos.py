c69b4defe0c45354ca9f3ab806f7609d4a7cb78c
Isaac <isaac.laaouaj@gmail.com>
2022-11-19 17:55:44 +0100

    Añado "Pregunta"

--- AgendaContactos.py
+++ AgendaContactos.py
@@ -4,11 +4,15 @@ def hola():
 def adios():
     print('adios')
 
+def pregunta():
+    print('Cómo estas?')
+
 
 mapa = {
     1:hola,
-    2:adios
+    2:adios,
+    3:pregunta
 }
 
-opcion = int(input('Dime la opcion:\n1: Saludar\n2: Despedirse\n'))
+opcion = int(input('Dime la opcion:\n1: Saludar\n2: Despedirse\n3: Pregunta\n'))
 mapa[opcion]()
