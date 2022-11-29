d38da90fd8fe391c17ac7824246aaf70cd8515ff
IsaacLaaouaj <isaac.laaouaj@gmail.com>
2022-11-29 15:11:28 +0100

    Correcciones

--- AgendaContactos.py
+++ AgendaContactos.py
@@ -1,5 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials,db
+
+#Funciones necesarias:
 def add_contact():
     name = input("Introduce el nombre del contacto: ")
     surname = input("Introduce el apellido: ")
@@ -17,14 +19,20 @@ def add_contact():
 def delete_contact():
     remove_contact = input("Introduce el nombre del contacto que quieres borrar: ")
 
+def modify_contact():
+    #Elige la opción que quieres modificar.
 
+
+#Firebase:
 credenciales = credentials.Certificate('agenda-1720b-firebase-adminsdk-nbaiz-0e38f4eba0.json')
 firebase_admin.initialize_app(credential=credenciales, options={"databaseURL" : "https://agenda-1720b-default-rtdb.europe-west1.firebasedatabase.app/"})
 ref = db.reference()
 
+#menu
 switcher = {
     1: add_contact,
-    2: delete_contact
+    2: delete_contact,
+    3: modify_contact
 }
 menu = int(input("Seleccione las opciones:\n1: Añadir contacto \n2: Borrar contacto"))  
 switcher[menu]()
