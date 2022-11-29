e9e8a3456ff0ec1780a854a06c0bf00e25c6b717
Jose Ventura <joseluis.ventura@nubika.com>
2022-11-29 15:45:14 +0100

    empiezo la funcion de pinta todos los contactos

--- AgendaContactos.py
+++ AgendaContactos.py
@@ -8,6 +8,7 @@ def add_contact():
     phone_number = input("Introduce el número de teléfono: ")
     address = input("Introduce la dirección: ")
     mail = input("Indroduce el correo electrónico: ")
+    
     ref.push({
         'Nombre':name,
         'Apellido': surname,
@@ -21,18 +22,25 @@ def delete_contact():
 
 def modify_contact():
     #Elige la opción que quieres modificar.
+    pass
+def all_contacts():
+    for contact in ref.get().values():
+        print('Nombre: ',contact['Nombre'])
+    
 
 
 #Firebase:
 credenciales = credentials.Certificate('agenda-1720b-firebase-adminsdk-nbaiz-0e38f4eba0.json')
 firebase_admin.initialize_app(credential=credenciales, options={"databaseURL" : "https://agenda-1720b-default-rtdb.europe-west1.firebasedatabase.app/"})
-ref = db.reference()
+ref = db.reference('contactos')
 
 #menu
 switcher = {
     1: add_contact,
     2: delete_contact,
-    3: modify_contact
+    3: modify_contact,
+    4: all_contacts
 }
 menu = int(input("Seleccione las opciones:\n1: Añadir contacto \n2: Borrar contacto"))  
 switcher[menu]()
+
