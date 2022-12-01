43a0f0d8d4eb9287475c473c751efba269a3462f
Jose Ventura <joseluis.ventura@nubika.com>
2022-12-01 22:26:23 +0100

    añadimos comentarios

--- programa/AgendaContactos.py
+++ programa/AgendaContactos.py
@@ -1,7 +1,8 @@
 import firebase_admin, sys, re
 from firebase_admin import credentials,db
 
-#Funciones necesarias:
+# Éste método recoge la información del contacto y después le hace una insercción o push a base de datos,
+# validando que el número de teléfono y el email cumpla el formato determinado.
 def add_contact():
     name = input("Introduce el nombre del contacto: ").lower()
     surname = input("Introduce el apellido: ").lower()
@@ -18,6 +19,12 @@ def add_contact():
     })
     print('\n'*20+'Contacto creado exitosamente!!')
 
+
+# Éste método llama a una función que busca un contacto en base al nombre que introduzcas,
+# al identificar al contacto que se quiera borrar lo borra en base de datos.
+#
+# Si hay varios contactos con un mismo nombre te va mostrando esos contactos hasta
+# identificar el correcto.
 def delete_contact():
     contactos = find_contact(False)
     for contacto in contactos:
@@ -39,6 +46,14 @@ def delete_contact():
             return
     print('\n'*20+'Ningún contacto ha sido borrado')
 
+
+# Éste método llama a una función que busca un contacto en base al nombre que introduzcas,
+# al identificar al contacto que se quiera modificar pregunta que campo quieres modificar.
+#
+# Una vez obtenido el campo a modificar y el dato lo edita en base de datos
+#
+# Si hay varios contactos con un mismo nombre te va mostrando esos contactos hasta
+# identificar el correcto.
 def modify_contact():
     contactos = find_contact(False)
     for contacto in contactos:
@@ -77,6 +92,10 @@ def modify_contact():
             print('\n'*20+'Contacto editado exitosamente!!')
             return
 
+
+# Ésta función hace una consulta en base de datos en base al nombre que tu busques
+# y dependiendo del parámetro print llama a una función de pintado de contactos o
+# te los devuelve en forma de lista
 def find_contact(print = True):
     name = input('Escribe nombre del contacto que quieras buscar: ').lower()
     contactos = ref.order_by_child('Nombre').equal_to(name).get()
@@ -85,7 +104,9 @@ def find_contact(print = True):
     else:
         return contactos
 
-def all_contacts():
+
+# Ésta funcion hace una consulta en base de datos,recoge todos
+# y llama a una función de pintado de contactos
     try:
         contactos = ref.get().values()
     except:
@@ -93,6 +114,9 @@ def all_contacts():
     else:
         print_contacts(contactos)
 
+
+# Ésta funcion recibe por parámetro una lista de contactos y la recorre
+# imprimiendo por pantalla cada contacto
 def print_contacts(contacts):
     for contact in contacts:
         print('---------------------------------------------------')
@@ -103,6 +127,10 @@ def print_contacts(contacts):
         print('Correo electronico: ', contact['Correo_electronico'])
         print('---------------------------------------------------')
 
+
+# Ésta funcion valida que el número de teléfono que le pasen
+# por parámetro tiene el formato adecuado y si no te vuelve a pedir
+# que lo introduzcas.
 def phone_number_valide(edit = False):
     if(edit):
         number = input("Introduce el número de teléfono: ")
@@ -116,6 +144,10 @@ def phone_number_valide(edit = False):
         number = re.findall(filter,number)
     return number[0]
 
+
+# Ésta funcion valida que el email que le pasen
+# por parámetro tiene el formato adecuado y si no te vuelve a pedir
+# que lo introduzcas.
 def mail_valide(edit = False):
     if(edit):
         mail = input("Introduce nuevo correo electrónico: ")
@@ -129,6 +161,9 @@ def mail_valide(edit = False):
         mail = re.findall(filter,mail)
     return mail[0]
 
+
+# Éste metodo valida que la opcion introducida en la edicion de un contacto
+# esté correcta.
 def edit_option_valide(error = False):
     if(error):
         print('ERROR!! Introduce una opcion valida\nQue campo quieres modificar?\n')
@@ -144,15 +179,18 @@ def edit_option_valide(error = False):
     else:
         return opcion
 
+
+# Ésta funcion para la ejecución del programa.
 def out():
     print('\n'*20+'HASTA PRONTO!!')
     sys.exit()
 
-#Firebase:
+# CONEXIÓN FIREBASE:
 credenciales = credentials.Certificate('programa/credenciales.json')
 firebase_admin.initialize_app(credential=credenciales, options={"databaseURL" : "https://agenda-1720b-default-rtdb.europe-west1.firebasedatabase.app/"})
 ref = db.reference('contactos')
-#menu
+
+# SWITCHER
 switcher = {
     1: add_contact,
     2: delete_contact,
@@ -161,9 +199,7 @@ switcher = {
     5: all_contacts,
     6: out
 }
-#menu = int(input("Seleccione las opciones:\n1: Añadir contacto \n2: Borrar contacto \n3: Modificar contacto \n4: Muestra todos los contactos \n5: Salir"))
-#switcher[menu]()
-
+# MENÚ
 while (True):
     try:
         menu = int(input("\nSeleccione las opciones:\n1: Añadir contacto \n2: Borrar contacto \n3: Modificar contacto \n4: Busca contacto \n5: Muestra todos los contactos \n6: Salir\n-> Introduce una opción: "))
